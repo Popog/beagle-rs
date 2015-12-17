5f200d5c54af2f2f902f741e96b67cb05b71c9fc
Popog <Popog42+github@gmail.com>
2015-12-16 23:52:13 -0800

    Added Sized constraint to Angles

--- src/angle.rs
+++ src/angle.rs
@@ -4,7 +4,7 @@
 use std::{f64,f32};
 
 /// Types that can be used with trigonometric functions
-pub trait Angle {
+pub trait Angle: Sized {
     /// The underlying type of the angle
     type Type : Into<Self>;
 
